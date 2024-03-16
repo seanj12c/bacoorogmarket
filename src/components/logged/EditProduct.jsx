d955a4356790cd81c1027a778b40b2567b1c103a
seanj12c <darkslayer0912@gmail.com>
2024-03-16 16:06:21 +0800

    Add loading

--- src/components/logged/EditProduct.jsx
+++ src/components/logged/EditProduct.jsx
@@ -9,6 +9,7 @@ import {
   StandaloneSearchBox,
 } from "@react-google-maps/api";
 import check from "../../assets/check.gif";
+import uploadload from "../../assets/loading.gif";
 
 const Modal = ({ show }) => {
   if (!show) {
@@ -49,6 +50,7 @@ const EditProduct = () => {
   const searchBoxRef = useRef(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const libraries = ["places"];
+  const [loading, setLoading] = useState(true);
 
   const [formData, setFormData] = useState({
     caption: "",
@@ -104,10 +106,6 @@ const EditProduct = () => {
             productData.location.latitude &&
             productData.location.longitude
           ) {
-            setSelectedLocation({
-              lat: productData.location.latitude,
-              lng: productData.location.longitude,
-            });
             setDefaultLocation({
               lat: productData.location.latitude,
               lng: productData.location.longitude,
@@ -120,6 +118,8 @@ const EditProduct = () => {
         }
       } catch (error) {
         console.error("Error getting product document:", error);
+      } finally {
+        setLoading(false);
       }
     };
     getProductData();
@@ -153,159 +153,182 @@ const EditProduct = () => {
   };
 
   return (
-    <div className="p-4 sm:p-6 md:p-8 lg:p-10">
-      <h2 className="text-2xl text-center font-bold text-primary pt-24 mb-3">
-        Edit Product
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="caption" className="text-lg text-primary">
-            Product Name
-          </label>
-          <input
-            type="text"
-            id="caption"
-            name="caption"
-            className="w-full border rounded p-2 mb-3"
-            placeholder="Name of your Product"
-            value={formData.caption}
-            onChange={handleChange}
+    <div>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <img
+            className="lg:h-32 h-20 md:h-28 object-contain mx-auto"
+            src={uploadload}
+            alt=""
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="description" className="text-lg text-primary">
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            className="w-full border rounded p-2 mb-3"
-            placeholder="Description of the Product"
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </div>
-        <h3 className="text-lg text-primary">Price</h3>
-        <div className="flex border rounded focus:outline-1 items-center w-28 h-full mb-3 pl-2">
-          <p className="font-bold">₱</p>
-          <input
-            required
-            type="number"
-            id="priceInput"
-            className="w-full focus:outline-none p-2 "
-            placeholder="Price"
-            style={{ WebkitAppearance: "none", MozAppearance: "textfield" }}
-            value={formData.price}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="address" className="text-lg text-primary">
-            Address
-          </label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            className="w-full border rounded p-2 mb-3"
-            placeholder="Address of the Product"
-            value={formData.address}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div style={{ height: "400px", width: "100%", marginBottom: "20px" }}>
-          <LoadScript
-            googleMapsApiKey="AIzaSyBLZJ-nDBC4jlEDsMb7qS3kjuJp90fTPbM"
-            libraries={libraries}
-          >
-            <StandaloneSearchBox
-              onLoad={(ref) => {
-                searchBoxRef.current = ref;
-              }}
-              onPlacesChanged={() => {
-                if (searchBoxRef.current) {
-                  const places = searchBoxRef.current.getPlaces();
-                  if (places.length > 0) {
-                    const { location } = places[0].geometry;
-                    setSelectedLocation({
-                      latitude: location.lat(),
-                      longitude: location.lng(),
-                    });
-                  }
-                }
-              }}
-            >
+      ) : (
+        <div className="p-4 sm:p-6 md:p-8 lg:p-10">
+          <h2 className="text-2xl text-center font-bold text-primary pt-24 mb-3">
+            Edit Product
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="caption" className="text-lg text-primary">
+                Product Name
+              </label>
               <input
-                className="text-lg text-black"
                 type="text"
-                placeholder="Search for a location"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                style={{
-                  boxSizing: "border-box",
-                  border: "1px solid transparent",
-                  width: "100%",
-                  height: "32px",
-                  padding: "0 12px",
-                  borderRadius: "3px",
-                  boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
-                  fontSize: "14px",
-                  outline: "none",
-                  textOverflow: "ellipses",
-                }}
+                id="caption"
+                name="caption"
+                className="w-full border rounded p-2 mb-3"
+                placeholder="Name of your Product"
+                value={formData.caption}
+                onChange={handleChange}
               />
-            </StandaloneSearchBox>
-            <GoogleMap
-              mapContainerStyle={{ height: "100%", width: "100%" }}
-              center={
-                selectedLocation
-                  ? {
-                      lat: selectedLocation.latitude,
-                      lng: selectedLocation.longitude,
-                    }
-                  : defaultLocation
-              }
-              zoom={13}
-              onClick={handleMapClick}
-              options={mapOptions}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="description" className="text-lg text-primary">
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                className="w-full border rounded p-2 mb-3"
+                placeholder="Description of the Product"
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </div>
+            <h3 className="text-lg text-primary">Price</h3>
+            <div className="flex border rounded focus:outline-1 items-center w-28 h-full mb-3 pl-2">
+              <p className="font-bold">₱</p>
+              <input
+                required
+                type="number"
+                id="priceInput"
+                className="w-full focus:outline-none p-2 "
+                placeholder="Price"
+                style={{ WebkitAppearance: "none", MozAppearance: "textfield" }}
+                value={formData.price}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="address" className="text-lg text-primary">
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="w-full border rounded p-2 mb-3"
+                placeholder="Address of the Product"
+                value={formData.address}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div
+              style={{ height: "400px", width: "100%", marginBottom: "20px" }}
             >
-              {selectedLocation && (
-                <Marker
-                  position={{
-                    lat: selectedLocation.latitude,
-                    lng: selectedLocation.longitude,
+              <LoadScript
+                googleMapsApiKey="AIzaSyBLZJ-nDBC4jlEDsMb7qS3kjuJp90fTPbM"
+                libraries={libraries}
+              >
+                <StandaloneSearchBox
+                  onLoad={(ref) => {
+                    searchBoxRef.current = ref;
                   }}
-                />
-              )}
-            </GoogleMap>
-          </LoadScript>
+                  onPlacesChanged={() => {
+                    if (searchBoxRef.current) {
+                      const places = searchBoxRef.current.getPlaces();
+                      if (places.length > 0) {
+                        const { location } = places[0].geometry;
+                        setSelectedLocation({
+                          latitude: location.lat(),
+                          longitude: location.lng(),
+                        });
+                      }
+                    }
+                  }}
+                >
+                  <input
+                    className="text-lg text-black"
+                    type="text"
+                    placeholder="Search for a location"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    style={{
+                      boxSizing: "border-box",
+                      border: "1px solid transparent",
+                      width: "100%",
+                      height: "32px",
+                      padding: "0 12px",
+                      borderRadius: "3px",
+                      boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
+                      fontSize: "14px",
+                      outline: "none",
+                      textOverflow: "ellipses",
+                    }}
+                  />
+                </StandaloneSearchBox>
+                <GoogleMap
+                  mapContainerStyle={{ height: "100%", width: "100%" }}
+                  center={
+                    selectedLocation
+                      ? {
+                          lat: selectedLocation.latitude,
+                          lng: selectedLocation.longitude,
+                        }
+                      : defaultLocation
+                  }
+                  zoom={13}
+                  onClick={handleMapClick}
+                  options={mapOptions}
+                >
+                  <Marker
+                    position={{
+                      lat: defaultLocation.lat,
+                      lng: defaultLocation.lng,
+                    }}
+                  />
+                  {selectedLocation && (
+                    <Marker
+                      position={{
+                        lat: selectedLocation.latitude,
+                        lng: selectedLocation.longitude,
+                      }}
+                    />
+                  )}
+                </GoogleMap>
+              </LoadScript>
+            </div>
+            <div className="mb-3 pt-10">
+              <label
+                htmlFor="otherInformation"
+                className="text-lg text-primary"
+              >
+                Other Information
+              </label>
+              <textarea
+                id="otherInformation"
+                name="otherInformation"
+                className="w-full border rounded p-2 mb-3"
+                placeholder="Other Information of the Product"
+                value={formData.otherInformation}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex  justify-center w-full">
+              <button
+                type="submit"
+                className="btn-wide btn btn-primary"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Updating..." : "Updating Product"}
+              </button>
+            </div>
+          </form>
+          <Modal show={isModalOpen} onClose={closeModal} />
         </div>
-        <div className="mb-3 pt-10">
-          <label htmlFor="otherInformation" className="text-lg text-primary">
-            Other Information
-          </label>
-          <textarea
-            id="otherInformation"
-            name="otherInformation"
-            className="w-full border rounded p-2 mb-3"
-            placeholder="Other Information of the Product"
-            value={formData.otherInformation}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex  justify-center w-full">
-          <button
-            type="submit"
-            className="btn-wide btn btn-primary"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Updating..." : "Updating Product"}
-          </button>
-        </div>
-      </form>
-      <Modal show={isModalOpen} onClose={closeModal} />
+      )}
     </div>
   );
 };
