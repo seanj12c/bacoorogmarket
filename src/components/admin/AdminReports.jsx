90c02f6d6422ef4794d3671132b80f781f0684a5
seanj12c <darkslayer0912@gmail.com>
2024-04-10 13:31:15 +0800

    Add profileReports

--- src/components/admin/AdminReports.jsx
+++ src/components/admin/AdminReports.jsx
@@ -28,14 +28,20 @@ const AdminReports = () => {
       const db = getFirestore();
       const reportsRef = collection(
         db,
-        reportType === "product" ? "productReports" : "recipeReports"
+        reportType === "profile"
+          ? "profileReports"
+          : reportType === "product"
+          ? "productReports"
+          : "recipeReports"
       );
       const querySnapshot = await getDocs(reportsRef);
-      const reportsData = querySnapshot.docs.map((doc) => doc.data());
+      const reportsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id, // Include the document ID in each report object
+        ...doc.data(),
+      }));
       setReports(reportsData);
       setLoading(false);
     };
-
     // Fetch products and registered users data
     const fetchAdditionalData = async () => {
       const db = getFirestore();
@@ -166,7 +172,7 @@ const AdminReports = () => {
               </ul>
             </div>
             {/* Reports List */}
-            <div className="container lg:w-4/5 md:w-4/5 md:ml-auto md:mr-0 mx-auto px-4">
+            <div className="container z-40 lg:w-4/5 md:w-4/5 md:ml-auto md:mr-0 mx-auto px-4">
               <div className="flex justify-center md:justify-end items-center gap-2 pt-1 mb-4">
                 <h1 className="text-xs">Type of Reports</h1>
                 <select
@@ -176,71 +182,164 @@ const AdminReports = () => {
                 >
                   <option value="product">Product Reports</option>
                   <option value="recipe">Recipe Reports</option>
+                  <option value="profile">Profile Reports</option>
                 </select>
               </div>
               <h1 className="text-center text-2xl font-bold">
-                {reportType === "product" ? "Product" : "Recipe"} Reports
+                {reportType === "product"
+                  ? "Product"
+                  : reportType === "recipe"
+                  ? "Recipe"
+                  : "Profile"}{" "}
+                Reports
               </h1>
               <ul className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 {reports.map((report, index) => (
-                  <li className="glass w-full card-body" key={index}>
+                  <li className="glass w-full p-5" key={index}>
                     {registeredUsers[report.userId] && (
                       <div className="flex items-center gap-2">
-                        <img
-                          className="md:w-14 md:h-14  w-10 h-10 border border-primary rounded-full object-cover"
-                          src={registeredUsers[report.userId].profilePhotoUrl}
-                          alt={`${registeredUsers[report.userId].firstName} ${
-                            registeredUsers[report.userId].lastName
-                          }`}
-                        />
-                        <p>
-                          {registeredUsers[report.userId].firstName}{" "}
-                          {registeredUsers[report.userId].lastName}
-                        </p>
+                        '
+                        <div>
+                          <img
+                            className="md:w-14 md:h-14  w-10 h-10 border border-primary rounded-full object-cover"
+                            src={registeredUsers[report.userId].profilePhotoUrl}
+                            alt={`${registeredUsers[report.userId].firstName} ${
+                              registeredUsers[report.userId].lastName
+                            }`}
+                          />
+                        </div>
+                        <div className="flex flex-col">
+                          <p>
+                            {registeredUsers[report.userId].firstName}{" "}
+                            {registeredUsers[report.userId].lastName}
+                          </p>
+                          <p className="italic text-error text-xs font-bold">
+                            The reported user
+                          </p>
+                        </div>
+                      </div>
+                    )}
+                    {reportType === "profile" && (
+                      <div className="flex flex-col gap-2">
+                        <div className="py-1 flex justify-end">
+                          <button
+                            className="btn btn-xs btn-primary"
+                            onClick={() =>
+                              document
+                                .getElementById(`profile-${report.id}`)
+                                .showModal()
+                            }
+                          >
+                            View Proof of Report
+                          </button>
+                          <dialog id={`profile-${report.id}`} className="modal">
+                            <div className="modal-box">
+                              <h3 className="font-bold text-center text-lg">
+                                Proof of Report
+                              </h3>
+                              <img
+                                className="w-full object-contain rounded-md"
+                                src={report.photoUrl}
+                                alt={`${
+                                  registeredUsers[report.userId].firstName
+                                } ${
+                                  registeredUsers[report.userId].lastName
+                                }'s proof of report`}
+                              />
+                            </div>
+                            <form method="dialog" className="modal-backdrop">
+                              <button>close</button>
+                            </form>
+                          </dialog>
+                        </div>
+                      </div>
+                    )}
+
+                    {reportType !== "profile" && (
+                      <div className="py-1 flex justify-end">
+                        <button
+                          className="btn btn-xs btn-primary"
+                          onClick={() => {
+                            const modalId =
+                              reportType === "product"
+                                ? `productModal-${report.productId}`
+                                : reportType === "recipe"
+                                ? `recipeModal-${report.recipeId}`
+                                : "";
+
+                            document.getElementById(modalId).showModal();
+                          }}
+                        >
+                          View Details
+                        </button>
                       </div>
                     )}
                     {reportType === "product" && products[report.productId] && (
                       <div>
                         <p>
-                          Product Name: {products[report.productId].caption}
+                          <span className="font-bold "> Product Name:</span>{" "}
+                          {products[report.productId].caption}
                         </p>
                       </div>
                     )}
                     {reportType === "recipe" && (
                       <div>
-                        <p>Recipe ID: {report.recipeId}</p>
+                        <p>
+                          <span className="font-bold ">Recipe ID: </span>
+                          {report.recipeId}
+                        </p>
                       </div>
                     )}
-                    <p>Reason: {report.reason}</p>
-                    <p>Explanation: {report.explanation}</p>
 
-                    <button
-                      className="btn"
-                      onClick={() => {
-                        const modalId =
-                          reportType === "product"
-                            ? `productModal-${report.productId}`
-                            : `recipeModal-${report.recipeId}`;
-                        document.getElementById(modalId).showModal();
-                      }}
-                    >
-                      View Image
-                    </button>
+                    <p>
+                      <span className="font-bold ">Reason: </span>
+                      {report.reason}
+                    </p>
+                    <p>
+                      <span className="font-bold ">Explanation: </span>{" "}
+                      {report.explanation}
+                    </p>
+                    {/* Conditionally render View Details button */}
                     <dialog
                       id={`productModal-${report.productId}`}
                       className="modal"
                     >
                       <div className="modal-box">
                         <h3 className="font-bold text-center text-lg">
-                          Photo Preview
+                          Product Details
                         </h3>
                         {reportType === "product" &&
                           products[report.productId] && (
                             <div className="text-center">
-                              <p className="font-bold italic">
+                              <p className="font-bold text-center italic">
                                 {products[report.productId].caption}
                               </p>
+                              <div className="py-2 text-left">
+                                <p>
+                                  <span className="font-bold">
+                                    Description{" "}
+                                  </span>
+                                  : {products[report.productId].description}
+                                </p>
+                                {/* Display Address */}
+                                <p>
+                                  <span className="font-bold">Address </span>:{" "}
+                                  {products[report.productId].address}
+                                </p>
+                                {/* Display Other Information */}
+                                <p>
+                                  <span className="font-bold">
+                                    Other Information{" "}
+                                  </span>
+                                  :{" "}
+                                  {products[report.productId].otherInformation}
+                                </p>
+                              </div>
+                              <div className="py-2">
+                                <h1 className="font-bold text-lg">Photos</h1>
+                              </div>
                               <div className="flex flex-col gap-2 justify-center items-center">
+                                {/* Display Photos */}
                                 {products[report.productId].photos.map(
                                   (photo, index) => (
                                     <img
@@ -266,7 +365,7 @@ const AdminReports = () => {
                     >
                       <div className="modal-box">
                         <h3 className="font-bold text-center text-lg">
-                          Recipe Photo Preview
+                          Recipe Details
                         </h3>
                         {reportType === "recipe" &&
                           recipes[report.recipeId] && (
@@ -274,7 +373,33 @@ const AdminReports = () => {
                               <p className="font-bold italic">
                                 {recipes[report.recipeId].caption}
                               </p>
+                              {/* Display Ingredients */}
+                              <div className="text-left py-2">
+                                <h4 className="font-bold">Ingredients:</h4>
+                                <ul className="list-disc pl-5">
+                                  {recipes[report.recipeId].ingredients.map(
+                                    (ingredient, index) => (
+                                      <li key={index}>{ingredient}</li>
+                                    )
+                                  )}
+                                </ul>
+                              </div>
+                              {/* Display Instructions */}
+                              <div className="text-left">
+                                <h4 className="font-bold">Instructions:</h4>
+                                <ol className="list-decimal pl-5">
+                                  {recipes[report.recipeId].instructions.map(
+                                    (instruction, index) => (
+                                      <li key={index}>{instruction}</li>
+                                    )
+                                  )}
+                                </ol>
+                              </div>
+                              <div className="py-2">
+                                <h1 className="font-bold text-lg">Photos</h1>
+                              </div>
                               <div className="flex flex-col gap-2 justify-center items-center">
+                                {/* Display Photos */}
                                 {recipes[report.recipeId].photos.map(
                                   (photo, index) => (
                                     <img
