6dec9e4efa23a4c0c7b108c741be09033eb38851
seanj12c <darkslayer0912@gmail.com>
2024-04-28 15:41:15 +0800

    Add view photo

--- src/components/admin/AdminInfo.jsx
+++ src/components/admin/AdminInfo.jsx
@@ -9,6 +9,7 @@ import {
 } from "firebase/firestore";
 import { IoReturnDownBackOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AdminInfo = () => {
   const { userId } = useParams();
@@ -66,11 +67,29 @@ const AdminInfo = () => {
     setShowProducts((prev) => !prev);
   };
 
+  const displayAllPhotos = (photos) => {
+    const photoElements = photos
+      .map(
+        (photo, idx) =>
+          `<img src="${photo}" alt="Image ${idx + 1}" class="w-full h-auto" />`
+      )
+      .join("");
+    Swal.fire({
+      html: photoElements,
+      showCloseButton: true,
+      showConfirmButton: false,
+      customClass: {
+        image: "custom-class-name",
+        closeButton: "btn btn-error btn-circle text-white",
+      },
+    });
+  };
+
   return (
     <div className="container mx-auto py-6 h-full flex flex-col justify-center">
       <div>
         <button
-          className="btn btn-xs md:btn-sm btn-error text-white btn-primary mb-4"
+          className="btn btn-xs md:btn-sm btn-error text-white btn-primary ml-4 mb-4"
           onClick={goBack}
         >
           Go Back <IoReturnDownBackOutline className="md:hidden" size={15} />
@@ -79,12 +98,12 @@ const AdminInfo = () => {
       </div>
       {userInfo && (
         <div className="flex flex-col items-center">
-          <div className="w-full flex items-center justify-center mb-4">
+          <div className="w-full px-2 flex items-center justify-center mb-4">
             <div>
               <img
                 src={userInfo.profilePhotoUrl}
                 alt="Profile"
-                className="rounded-full mx-auto w-52 h-52 object-cover border border-full"
+                className="rounded-full mx-auto h-32 w-32 md:w-52 md:h-52 object-cover border border-full"
               />
               <h2 className="text-2xl text-center font-semibold mt-4">
                 {userInfo.firstName} {userInfo.lastName}
@@ -118,42 +137,46 @@ const AdminInfo = () => {
               Recipes
             </button>
           </div>
+
           <div className="w-full">
             {showProducts ? (
               <div className="p-4">
                 <h2 className="text-2xl font-semibold mb-4">Products</h2>
-                <div className="overflow-y-auto max-h-96">
+                <h1 className="text-xs italic text-primary">
+                  Tap specific product to see all photos from that post in full
+                  screen
+                </h1>
+                <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                   {userProducts.map((product, index) => (
                     <div
                       key={index}
-                      className="border-b border-gray-300 mb-4 pb-4"
+                      className="border flex flex-col justify-around glass border-gray-300 p-4"
+                      onClick={() => displayAllPhotos(product.photos)}
                     >
-                      <h3 className="text-lg font-semibold">
-                        {product.caption}
-                      </h3>
-                      <p>{product.description}</p>
-                      <p>
-                        <strong>Address:</strong> {product.address}
-                      </p>
-                      <p>
-                        <strong>Price:</strong> {product.price}
-                      </p>
-                      <p>
-                        <strong>Timestamp:</strong> {product.timestamp}
-                      </p>
                       <div>
-                        <strong>Photos:</strong>
-                        <ul className="grid grid-cols-2 gap-4">
-                          {product.photos.map((photo, idx) => (
-                            <li key={idx}>
-                              <img
-                                src={photo}
-                                alt={`Product ${idx + 1}`}
-                                className="w-full h-auto"
-                              />
-                            </li>
-                          ))}
-                        </ul>
+                        {" "}
+                        <h3 className="text-lg font-semibold">
+                          {product.caption}
+                        </h3>
+                        <p>{product.description}</p>
+                        <div>
+                          <img
+                            src={product.photos[0]}
+                            alt={`Product 1`}
+                            className="w-full h-auto"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <p>
+                          <strong>Address:</strong> {product.address}
+                        </p>
+                        <p>
+                          <strong>Price:</strong> {product.price}
+                        </p>
+                        <p>
+                          <strong>Timestamp:</strong> {product.timestamp}
+                        </p>
                       </div>
                     </div>
                   ))}
@@ -162,47 +185,49 @@ const AdminInfo = () => {
             ) : (
               <div className="p-4">
                 <h2 className="text-2xl font-semibold mb-4">Recipes</h2>
-                <div className="overflow-y-auto max-h-96">
+                <h1 className="text-xs italic text-primary">
+                  Tap specific recipes to see all photos from that post in full
+                  screen
+                </h1>
+                <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                   {userRecipes.map((recipe, index) => (
                     <div
                       key={index}
-                      className="border-b border-gray-300 mb-4 pb-4"
+                      className="border flex flex-col justify-around glass border-gray-300 p-4"
+                      onClick={() => displayAllPhotos(recipe.photos)}
                     >
-                      <h3 className="text-lg font-semibold">
-                        {recipe.caption}
-                      </h3>
-                      <p>
-                        <strong>Timestamp:</strong> {recipe.timestamp}
-                      </p>
                       <div>
-                        <strong>Ingredients:</strong>
-                        <ul>
-                          {recipe.ingredients.map((ingredient, idx) => (
-                            <li key={idx}>{ingredient}</li>
-                          ))}
-                        </ul>
+                        <h3 className="text-lg font-semibold">
+                          {recipe.caption}
+                        </h3>
+                        <div>
+                          <img
+                            src={recipe.photos[0]}
+                            alt={`Recipe 1`}
+                            className="w-full h-auto"
+                          />
+                        </div>
                       </div>
                       <div>
-                        <strong>Instructions:</strong>
-                        <ul>
-                          {recipe.instructions.map((instruction, idx) => (
-                            <li key={idx}>{instruction}</li>
-                          ))}
-                        </ul>
-                      </div>
-                      <div>
-                        <strong>Photos:</strong>
-                        <ul className="grid grid-cols-2 gap-4">
-                          {recipe.photos.map((photo, idx) => (
-                            <li key={idx}>
-                              <img
-                                src={photo}
-                                alt={`Recipe ${idx + 1}`}
-                                className="w-full h-auto"
-                              />
-                            </li>
-                          ))}
-                        </ul>
+                        <p>
+                          <strong>Timestamp:</strong> {recipe.timestamp}
+                        </p>
+                        <div>
+                          <strong>Ingredients:</strong>
+                          <ul>
+                            {recipe.ingredients.map((ingredient, idx) => (
+                              <li key={idx}>{ingredient}</li>
+                            ))}
+                          </ul>
+                        </div>
+                        <div>
+                          <strong>Instructions:</strong>
+                          <ul>
+                            {recipe.instructions.map((instruction, idx) => (
+                              <li key={idx}>{instruction}</li>
+                            ))}
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   ))}
